c57c9eea69843ef8c0a2727e49428162d76c02cc
iMokhles <imokhles@MacBook-Air-de-iMokhles.local>
2018-11-03 11:24:20 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # UITemplate-iOS
 Swift application with most of my applications ui
+
+### Screenshots
+
+<h3 align="center">
+<img src="screenshots/screen1.png" alt="Screenshot 1" />
+</h3>
+
+<h3 align="center">
+<img src="screenshots/screen2.png" alt="Screenshot 2" />
+</h3>
---
+++ UITemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITemplate
+//
+//  Created by iMokhles on 02/11/2018.
+//  Copyright © 2018 iMokhles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
